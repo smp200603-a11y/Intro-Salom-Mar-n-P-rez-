f038aaeb2e2eef99575ceec3e1b4c0a8459e94fe
smp200603-a11y <smp200603@gmail.com>
2026-02-26 13:32:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ st.image(image,caption="snoopy-joe-cool-e1725621027692.jpg")
 
 texto=st.text_input("Ingresa texto","texto inicial")
 st.write("El texto que has escrito es",texto)
-if st.button("presiona el boton"):
-   st.write("has presionado")
+if st.button("Presiona el boton"):
+   st.write("Has presionado")
