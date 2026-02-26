35cb3e9ace2d369d41466ad390dc96a9f9678255
smp200603-a11y <smp200603@gmail.com>
2026-02-26 13:24:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from PIL import Image
 st.title("Mi primera app")
 st.header("Esta es mi pagina de presentación")
 
-Image=Image.open("snoopy-joe-cool-e1725621027692.jpg")
+image=Image.open("snoopy-joe-cool-e1725621027692.jpg")
 st.image(image,caption="snoopy-joe-cool-e1725621027692.jpg")
