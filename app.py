dbeac0c64fa32c6ccf06669cbba958a4f07e0317
smp200603-a11y <smp200603@gmail.com>
2026-02-26 13:18:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 import streamlit as st 
+from PIL import Image
 st.title("Mi primera app")
 st.header("Esta es mi pagina de presentación")
+
+Image=Image.open("myimagen.snoopy-joe-cool-e1725621027692")
